16b44af3b17a85889851aafeb46966ae5f3c1dfd
Kushal Banda <kushalbanda265@gmail.com>
2024-06-08 21:10:59 +0530

    Folders  added

--- template.py
+++ template.py
@@ -2,28 +2,23 @@ import os
 from pathlib import Path
 import logging
 
+package_name = 'mongodb_connect'
+
 
 list_of_files = [
     ".github/workflows/.gitkeep",
     "src/__init__.py",
-    "src/components/data_ingestion.py",
-    "src/components/data_transformation.py",
-    "src/components/model_trainer.py",
-    "src/components/model_evaluation.py",
-    "src/pipeline/__init__.py",
-    "src/pipeline/training_pipeline.py",
-    "src/pipeline/prediction_pipeline.py",
-    "src/utils/__init__.py",
-    "src/utils/utils.py",
-    "src/logger/logger.py",
-    "src/exceptions/exceptions.py",
+    f"src/{package_name}/__init__.py",
+    f"src/{package_name}/mongo_crud.py",
+    "tests/__init__.py",
     "tests/unit/__init__.py",
+    "tests/unit/unit.py",
     "tests/integration/__init__.py",
     "init_setup.sh",
     "requirements.txt",
     "requirements_dev.txt",
     "setup.py",
-    "setup.config",
+    "setup.cfg",
     "pyproject.toml",
     "tox.ini",
     "experiment/experiments.ipynb"
